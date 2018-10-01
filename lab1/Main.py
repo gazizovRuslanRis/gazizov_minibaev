8dae37cadd08094f9f2096fae0d06f606b196aa3
Ильназ <kpa6ctep95@gmail.com>
2018-10-01 10:27:27 +0500

    Работа сдана (indent = 2)

--- lab1/Main.py
+++ lab1/Main.py
@@ -17,62 +17,62 @@ for s in wb.sheets():
             except : pass
             if (value != ''):
                 col_value.append(value)
-                values.append(col_value)
+        values.append(col_value)
 
-result = re.sub(r'(\[\], )|[\[\]\']|(\s){2,}', '', str(values))
-result = re.sub(r'(, ,)', ',', result)
-result = re.sub(r'\\n|\\xa0', ' ', result)
-print(result)
+    result = re.sub(r'(\[\], )|[\[\]\']|(\s){2,}', '', str(values))
+    result = re.sub(r'(, ,)', ',', result)
+    result = re.sub(r'\\n|\\xa0', ' ', result)
+    #print(result)
 
-days = ['понедельник',
-        'вторник',
-        'среда',
-        'четверг',
-        'пятница',
-        'суббота']
+    days = ['понедельник',
+            'вторник',
+            'среда',
+            'четверг',
+            'пятница',
+            'суббота']
 
-times = ['8:00' ,
-         '9:40' ,
-         '11:30',
-         '13:20',
-         '15:00',
-         '16:40']
+    times = ['8:00' ,
+             '9:40' ,
+             '11:30',
+             '13:20',
+             '15:00',
+             '16:40']
 
-dict = {'понедельник' : '',
-        'вторник'     : '',
-        'среда'       : '',
-        'четверг'     : '',
-        'пятница'     : '',
-        'суббота'     : ''}
+    dict = {'понедельник' : '',
+            'вторник'     : '',
+            'среда'       : '',
+            'четверг'     : '',
+            'пятница'     : '',
+            'суббота'     : ''}
 
-dict_times = {'8:00' : '',
-             '9:40'  : '',
-             '11:30' : '',
-             '13:20' : '',
-             '15:00' : '',
-             '16:40' : ''}
+    dict_times = {'8:00' : '',
+                 '9:40'  : '',
+                 '11:30' : '',
+                 '13:20' : '',
+                 '15:00' : '',
+                 '16:40' : ''}
 
-for d in range(len(days)):
-    if d == (len(days)-1):
-        temp = re.search(r'(?<=%s, ).+' % (days[d]), result)
-        str = ''.join(temp[0])
-    else:
-        temp = re.search(r'(?<=%s, ).+(?=, %s)' % (days[d], days[d+1]), result)
-        str = ''.join(temp[0])
-
-    for t in range(len(times)):
-        if t == (len(times)-1):
-            tmp = re.findall(r'(?<=%s, ).+' % (times[t]), str)
-            dict_times[times[t]] = tmp
+    for d in range(len(days)):
+        if d == (len(days)-1):
+            temp = re.search(r'(?<=%s, ).+' % (days[d]), result)
+            str = ''.join(temp[0])
         else:
-            tmp = re.findall(r'(?<=%s, ).+(?=, %s)' % (times[t], times[t + 1]), str)
-            dict_times[times[t]] = tmp
-    dict[days[d]] = dict_times.copy()
+            temp = re.search(r'(?<=%s, ).+(?=, %s)' % (days[d], days[d+1]), result)
+            str = ''.join(temp[0])
 
-for key in dict:
-    print('\n' + key)
-    for subkey in dict[key]:
-        print('%s -> %s' % (subkey, dict[key][subkey]))
+        for t in range(len(times)):
+            if t == (len(times)-1):
+                tmp = re.findall(r'(?<=%s, ).+' % (times[t]), str)
+                dict_times[times[t]] = tmp
+            else:
+                tmp = re.findall(r'(?<=%s, ).+(?=, %s)' % (times[t], times[t + 1]), str)
+                dict_times[times[t]] = tmp
+        dict[days[d]] = dict_times.copy()
 
-with open('data.json', 'w') as f:
-    json.dump(dict, f, ensure_ascii=False)
+    for key in dict:
+        print('\n' + key)
+        for subkey in dict[key]:
+            print('%s -> %s' % (subkey, dict[key][subkey]))
+
+    with open('data.json', 'w') as f:
+        json.dump(dict, f, ensure_ascii=False, indent=2)
